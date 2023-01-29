44c0a7a12cf38ca4aad68c118173791cb451359f
Douglas-Cezaro <cezarodouglas@gmail.com>
2023-01-29 16:58:15 -0300

    test

--- src/app.ts
+++ src/app.ts
@@ -7,17 +7,17 @@ const app = express();
 const port = 80;
 app.use(express.json());
 
-const result = require("dotenv").config({
-  path: "./.env",
-});
+// const result = require("dotenv").config({
+//   path: "./.env",
+// });
 
-if (result.error) {
-  throw result.error;
-}
+// if (result.error) {
+//   throw result.error;
+// }
 app.get("/test", [], async (req, res) => {
   try {
     // const data = await getConnection.getRepository(UserEntity).find();
-    return res.status(200).send({ status: "OK", data: process.env.DB_PASS });
+    return res.status(200).send({ status: "OK" });
   } catch (error) {
     console.log(error);
     return res.status(500).send({ error });
