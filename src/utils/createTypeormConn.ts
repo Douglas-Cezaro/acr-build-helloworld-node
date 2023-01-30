de3d85c4ab872206ef5dd5588fa37a7565069ea7
Douglas-Cezaro <cezarodouglas@gmail.com>
2023-01-30 11:13:32 -0300

    test ENV

--- src/utils/createTypeormConn.ts
+++ src/utils/createTypeormConn.ts
@@ -1,5 +1,7 @@
 import { DataSource } from "typeorm";
 const ormConfig = require("../../ormconfig.js");
+console.log(process.env.DB_PASS);
+
 export const createTypeormConn = new DataSource(
   ormConfig.find((element) => element.name == process.env.NODE_ENV.trim())
 );
